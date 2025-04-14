b6e28ed8c8f8a60240c542b1e276417169868fd2
YanuarEka <yanuarekasusila@gmail.com>
2025-04-14 19:49:29 +0700

    feat: setup dexie js

---
+++ src/lib/dexie.ts
@@ -0,0 +1,27 @@
+import Dexie, { Table } from 'dexie';
+
+interface DEX_Thread {
+  id: string;
+  title: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
+class ChatDB extends Dexie {
+  threads!: Table<DEX_Thread>;
+
+  constructor() {
+    super('chatdb');
+
+    this.version(1).stores({
+      threads: 'id, title, created_at, updated_at',
+    });
+
+    this.threads.hook('creating', (_, obj) => {
+      obj.created_at = new Date();
+      obj.updated_at = new Date();
+    });
+  }
+}
+
+export const db = new ChatDB();
